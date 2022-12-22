bf27ca385703b77653b733dfdd824760f1c9fac4
ian901204 <scckate444@gmail.com>
2022-12-23 01:46:34 +0800

    fix sql bug

--- data_object/student.php
+++ data_object/student.php
@@ -2,15 +2,17 @@
 require_once __DIR__."/mysql_conn.php";
 class student extends mysql_conn
 {
+    private static $table = "student";
     function __construct() {
         parent::__construct();
     }
     public function insert($id, $name, $major_id){
         try{
+            $table = $this::$table;
             $id = $this::$conn -> quote($id);
             $name = $this::$conn -> quote($name);
             $major_id = $this::$conn -> quote($major_id);
-            $result = $this::$conn -> exec("insert into student VALUES($id, $name, $major_id)");
+            $result = $this::$conn -> exec("INSERT INTO $table VALUES($id, $name, $major_id)");
             if ($result == 0){
                 return FALSE;
             }elseif ($result == 1) {
@@ -22,15 +24,17 @@ class student extends mysql_conn
     }
     public function get_all(){
         try{
-            return $this::$conn -> query("SELECT * FROM student") -> fetchAll();
+            $table = $this::$table;
+            return $this::$conn -> query("SELECT * FROM $table") -> fetchAll();
         }catch (PDOException $e){
             return $e -> getMessage();
         }
     }
     public function get_by_id($id){
         try{
+            $table = $this::$table;
             $id = $this::$conn -> quote($id);
-            $data = $this::$conn -> query("SELECT * FROM student WHERE id = $id");
+            $data = $this::$conn -> query("SELECT * FROM $table WHERE id = $id");
             return $data -> fetchAll();
         }catch (PDOException $e){
             return $e -> getMessage();
@@ -38,8 +42,9 @@ class student extends mysql_conn
     }
     public function delete_by_id($id){
         try{
+            $table = $this::$table;
             $id = $this::$conn -> quote($id);
-            $result = $this::$conn -> exec("delete from student where id = $id");
+            $result = $this::$conn -> exec("DELETE FROM $table WHERE id = $id");
             if ($result == 0){
                 return FALSE;
             }elseif ($result == 1){
@@ -58,16 +63,12 @@ class student extends mysql_conn
         // ]
         try{
             $id = $this::$conn -> quote($id);
-            $sql = "UPDATE student SET name=:name, major_id=:major_id where id = $id";
+            $sql = "UPDATE $table SET name=:name, major_id=:major_id where id = $id";
             $sql = $this::$conn -> prepare(sql);
             $result = $sql -> execute($update_data);
         }catch(PDOException $e){
             return $e -> getMessage();
         }
-    }
-    public function sqlQuery($sql){
-        $sql = $this::$conn -> prepare($sql);
-        return $sql -> execute();
     }
 }
 ?>
